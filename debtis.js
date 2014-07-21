71b50ba1415f8c65616a4b42d743181da4ed9a61
Karissa McKelvey <krmckelv@gmail.com>
2014-07-20 21:12:27 -0700

    ENH: basic updates to the site, including making the html simpler, removing flatui in preparation for some design work and improving the javascript modularity and errorchecking

--- debtis.js
+++ debtis.js
@@ -1,15 +1,17 @@
 var app = angular.module('myDebtIs', []);
 
 app.controller('myDebtIsCtrl', function ($scope) {
-
-    $scope.debt = 0;
-    $scope.total = 0;
-    $scope.secondaryMarketDebt = 0;
     $scope.debtType = 'salliemae';
 
-    $scope.BEGINNING_PRINCIPAL = 0;
-    $scope.ESTIMATED_INTEREST = .068;
-    $scope.ESTIMATED_YEARS = 20;
+    $scope.resetState = function() {
+        $scope.total = 0;
+        $scope.secondaryMarketDebt = 0;
+        $scope.totalInterest = 0;
+        $scope.BEGINNING_PRINCIPAL = 0;
+        $scope.ESTIMATED_INTEREST = .068;
+        $scope.ESTIMATED_YEARS = 20;
+    }
+    $scope.resetState()
 
     var getDebtMarketPrice = function() {
         var price = 1
@@ -21,23 +23,30 @@ app.controller('myDebtIsCtrl', function ($scope) {
                 price = .15
                 break
         }
-        return Math.round($scope.total * price)
+        return $scope.BEGINNING_PRINCIPAL * price
     }
 
     var calculateDebt = function(debt, interest, yearsLeft) {
         if (yearsLeft <= 0) {
-            return $scope.BEGINNING_PRINCIPAL + interest
+            $scope.total = $scope.BEGINNING_PRINCIPAL + interest
+            $scope.totalInterest = interest
+            $scope.secondaryMarketDebt = getDebtMarketPrice()
+            return
         }
         var interestPaid = debt * $scope.ESTIMATED_INTEREST
         return calculateDebt(debt - interestPaid, interest + interestPaid, yearsLeft - 1)
     }
 
     $scope.submit = function() {
-        debt = parseFloat($scope.debt)
+        debt = parseFloat($scope.debt.replace(',', ''))
         if (!isNaN(debt) && debt != '') {
             $scope.BEGINNING_PRINCIPAL = debt
-            $scope.total = calculateDebt(debt, 0, $scope.ESTIMATED_YEARS);
-            $scope.secondaryMarketDebt = getDebtMarketPrice()
+            calculateDebt(debt, 0, $scope.ESTIMATED_YEARS);
+            $scope.showValues = true
+        }
+        else {
+            $scope.showValues = false
+            $scope.resetState()
         }
     };
 });
@@ -45,7 +54,7 @@ app.controller('myDebtIsCtrl', function ($scope) {
 app.filter('humanize', function(){
     return function humanize(number) {
         if(number < 1000) {
-            return number;
+            return Math.round(number);
         }
         var si = ['K', 'M', 'G', 'T', 'P', 'H'];
         var exp = Math.floor(Math.log(number) / Math.log(1000));
