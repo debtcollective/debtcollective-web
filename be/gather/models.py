aef096fe98b61cb174b26b90d17d0d2cc08a654e
Karissa McKelvey <krmckelv@gmail.com>
2014-09-01 13:50:12 -0700

    ENH: do the models for the information gathering app

--- be/gather/models.py
+++ be/gather/models.py
@@ -2,14 +2,30 @@ from django.db import models
 
 # Create your models here.
 
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    optional = models.BooleanField(default=True)
+class UserData(models.Model):
+  user_id_secret = models.CharField(max_length=200)
+  created_at = models.DateTimeField()
 
-class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+  @staticmethod
+  def user_id_secret(user_id):
+    """TODO: encrypt the given user id
+    to create the user_id_secret"""
+    pass
+
+class Debt(models.Model):
+  AUTO = 1
+  HOME = 2
+  STUDENT = 3
+  CREDIT = 4
+  DEBT_CHOICES = (
+    (AUTO, 'Auto'),
+    (HOME, 'Home'),
+    (STUDENT, 'Student'),
+    (CREDIT, 'Credit')
+  )
+
+  userdata = ForeignKey(UserData)
+  amount = models.IntegerField()
+  kind = models.IntegerField(max_length=1, choices=DEBT_CHOICES)
 
 
