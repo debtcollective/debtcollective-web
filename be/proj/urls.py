6f36e1687393afb4191f847ffa1e88290ab3bebc
Karissa McKelvey <krmckelv@gmail.com>
2014-09-05 21:43:38 -0700

    ENH/REF: add base template for genericity across multiple content templates

--- be/proj/urls.py
+++ be/proj/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', 'be.proj.views.splash', name='splash')
+    url(r'^$', 'be.proj.views.splash', name='splash'),
 )
