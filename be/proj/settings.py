7fd6f9aa46f725bf82557a41249e289a1a748697
Karissa McKelvey <krmckelv@gmail.com>
2014-09-13 17:07:19 -0400

    DOC/STL: small fixups and documentation additions

--- be/proj/settings.py
+++ be/proj/settings.py
@@ -6,29 +6,27 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
+
+See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Build paths inside the project
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'nuu3(*jjx3meo@nn14j58z25&2%ofkb0ktp6=e0m$=*rfz-myl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
+# Override the above with environment keys generated on the production server
+from envconfig import get_envconfig
+get_envconfig(globals())
 
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,7 +37,6 @@ INSTALLED_APPS = (
     'be.proj',
     'be.proj.gather'
 )
-
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,15 +45,12 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
-
 ROOT_URLCONF = 'be.proj.urls'
-
+# XXX: unsure why 'be' needs to be left out of the initial path (karissa)
 WSGI_APPLICATION = 'proj.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -70,21 +64,14 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
 
 # Parse database configuration from $DATABASE_URL
@@ -104,13 +91,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates').replace('\\','/'),
 )
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+# Misc.
 AUTH_PROFILE_MODULE = 'gather.UserProfile'
--- be/proj/static/js/maps/debtMap.js
+++ be/proj/static/js/maps/debtMap.js
@@ -1,54 +1,68 @@
 app.controller('mapCtrl', function ($scope, $http, util_svc) {
     var map = new AmCharts.AmMap();
 
-    map.pathToImages = "/static/js/vendor/ammap/images/";
-
-    // square
-    var targetSVG = "M-4.5,5.5 L5.5,5.5 L5.5,-4.5 L-4.5,-4.5 Z";
-
-    var dataProvider = {
-        map: "worldHigh"
-    };
-
-    map.addListener("clickMapObject", function (event) {
-        console.log(event);
-        if (event.mapObject.id != undefined && chartData[event.mapObject.id] != undefined) {
-            console.log(event.mapObject.id);
-        }
-    });
-
+    /************
+    MAP SETTINGS
+    ************/
     map.areasSettings = {
         autoZoom: true,
         selectedColor: "#CC0000"
     };
 
+    map.pathToImages = "/static/js/vendor/ammap/images/";
     map.useHandCursorOnClickableOjects = true
+
+    // hand drawn look & feel
     map.handDrawn = true;
     map.handDrawScatter = 3;
     map.handDrawThickness = 4;
 
+    // square
+    var targetSVG = "M-4.5,5.5 L5.5,5.5 L5.5,-4.5 L-4.5,-4.5 Z";
+
+    // get data
+    var dataProvider = {
+        map: "worldHigh"
+    };
     $http.get('/map_data/').then(function (resp) {
-        parsedImages = [];
-        for (idx in resp.data) {
-            point = resp.data[idx];
-            parsedImages.push(amChartPoint(point));
+        var parsedImages = [];
+        var total_users = resp.data.total_users;
+
+        var point;
+        for (idx in resp.data.points) {
+            point = createAmChartPoint(resp.data[idx]);
+            point['scale'] = getPointScale(point['num_users']/total_users);
+            parsedImages.push(point);
         }
+
         dataProvider['images'] = parsedImages;
         map.dataProvider = dataProvider;
         map.validateNow();
         map.write("mapdiv");
     });
 
-    function pointTitle(point) {
+    /**********
+     FUNCTIONS
+    ***********/
+    function getPointScale(percentage) {
         /*
-        Creates a title for the given point.
-        e.g.
-        Pasadena
-        1 debtor
-
-        New York
-        32 debtors
+        Gets the point's scale based on the percentage of
+        users in that location vs. all locations.
         */
+
+        // TODO
+        return percentage;
+    }
+
+    function getPointTitle(point) {
+        /*
+        Creates a title for the given point. Examples:
+        "Pasadena
+         1 debtor"
+        "New York
+         32 debtors"
+        */
+
         var suffix = " debtor";
         if (point['num_users'] > 1) {
             suffix += 's';
@@ -56,7 +70,7 @@ app.controller('mapCtrl', function ($scope, $http, util_svc) {
         return point['name'] + "<br>" + point['num_users'] + suffix;
     }
 
-    function amChartPoint(point) {
+    function createAmChartPoint(point) {
         /*
         Creates an amChart point from the given point
         that is given to us in the backend.
@@ -69,10 +83,19 @@ app.controller('mapCtrl', function ($scope, $http, util_svc) {
             name
             num_users
         */
-        point['title'] = pointTitle(point);
+
+        point['title'] = getPointTitle(point);
         point['svgPath'] = targetSVG;
         point['color'] = '#B00000';
-        point['scale'] = Math.min(point['num_users'], 3)
         return point
     }
+
+    // TODO: on map object click
+    map.addListener("clickMapObject", function (event) {
+        console.log(event);
+        if (event.mapObject.id != undefined && chartData[event.mapObject.id] != undefined) {
+            console.log(event.mapObject.id);
+        }
+    });
+
 });
