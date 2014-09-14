bb42ba21f9fb7906f2c581ed33d9795079d82347
Karissa McKelvey <krmckelv@gmail.com>
2014-09-14 19:16:52 -0400

    CLN: unnecessary settings line

--- be/proj/settings.py
+++ be/proj/settings.py
@@ -71,10 +71,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_URL = '/static/'
-
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 config = dj_database_url.config()
