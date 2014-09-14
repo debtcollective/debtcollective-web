644040c04ed679d95b2375b2d9ada1a0bcd73055
Karissa McKelvey <krmckelv@gmail.com>
2014-09-14 19:23:40 -0400

    BUG: trailing comma

--- be/proj/settings.py
+++ be/proj/settings.py
@@ -97,7 +97,7 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates').replace('\\','/'),
 )
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Misc.
