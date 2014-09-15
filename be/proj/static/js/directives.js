d5d49da87cdd9a87fe6e7e9c09c62941dd1f7424
Karissa McKelvey <krmckelv@gmail.com>
2014-09-15 01:12:09 -0400

    ENH: make background colors shift nicely

--- be/proj/static/js/directives.js
+++ be/proj/static/js/directives.js
@@ -3,15 +3,27 @@ app.directive("scroll", function ($window) {
 
         var OFFSET = 100;
         var MAX_RANGES = 21;
+        var RANGE_COLORS = {
+            15: "#281519",
+            16: "#2e2327",
+            17: "#393035",
+            18: "#454b49",
+            19: "#3f4543",
+            20: "#38413e",
+            21: "#39413F"
+        }
+
 
         //$scope.one = true;
         $scope.yLoc = 0;
         $scope.ranges = []
+        $scope.locLookup = {}
 
         function createRange() {
             var i = 0;
             var cur = 0;
             while (i < MAX_RANGES) {
+                $scope.locLookup[cur] = i;
                 $scope.ranges.push({
                     'yScroll': cur,
                     'num': i
@@ -21,23 +33,28 @@ app.directive("scroll", function ($window) {
             }
         }
 
-        createRange()
+        createRange();
+
+        $scope.$watch('yLoc', function(newVal, oldVal) {
+            i = $scope.locLookup[newVal];
+            $scope.backgroundStyle = {"background-color" : RANGE_COLORS[i]};
+        });
 
         $scope.advance = function () {
             $scope.yLoc = $scope.yLoc + OFFSET;
         }
 
-        function advancingFunction(i) {
+        function scrolling(i) {
             if (this.pageYOffset >= i * Math.floor(this.pageYOffset/OFFSET)*OFFSET) {
                 $scope.yLoc = i * Math.ceil(this.pageYOffset/OFFSET)*OFFSET;
             }
             else {
-                advancingFunction(i + 1);
+                scrolling(i + 1);
             }
         }
 
         angular.element($window).bind("scroll", function() {
-            advancingFunction(1);
+            scrolling(1);
             $scope.$apply();
         });
     };
