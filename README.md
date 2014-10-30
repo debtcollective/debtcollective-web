ad0a2cdea00606bb6d46158ce3e64657d26f1560
Karissa McKelvey <krmckelv@gmail.com>
2014-10-30 15:55:33 -0700

    Removing sensitive data/passwords

---
+++ README.md
@@ -0,0 +1,80 @@
+debt.is and debtcollective.org
+=======
+
+This is a django site deployed on the heroku framework.
+
+Please fork the repository and open pull requests when you're ready to merge.
+
+### Step 1: Install postgresql
+
+*Mac OSX*
+http://postgresapp.com/
+
+*Linux*
+I trust you can figure it out
+
+*Windows*
+Good luck
+
+On the terminal:
+```
+$ createdb debtis
+$ createuser debtis
+```
+
+### Step 2: Install Python and Virtualenv
+
+Follow instructions for your platform.
+
+### Step 3: Setup environment
+
+On the terminal:
+```
+$ git clone </path/to/repo>
+$ cd <repo>
+$ virtualenv venv
+$ export PYTHONPATH=$PYTHONPATH:`pwd`
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+$ cd be
+$ mkdir -p staticfiles/js
+$ python manage.py syncdb
+```
+
+### Step 4: Set up point data
+
+This loads the points (world cities) into the database. This small set is just for testing. There's a larger file that has the full list to be used in production.
+
+```
+$ cat points-small.sql | python manage.py dbshell
+```
+
+## Running the app
+
+```
+$ source venv/bin/activate
+(venv) $ cd be
+(venv) $ python manage.py runserver
+```
+
+## Testing the app
+
+Create the postgresql user and databases debtis
+```
+$ psql
+psql> CREATE DATABASE test_debtis;
+psql> ALTER USER debtis createdb;
+psql> ALTER DATABASE test_debtis OWNER TO debtis;
+```
+
+Running the tests:
+```
+$ python manage.py test
+```
+
+## Updating the map
+
+Visit this in your browser:
+```
+http://localhost:8000/generate_map_json/?password=MAGIC_PASSWORD
+```
---
+++ be/proj/gather/views.py
@@ -0,0 +1,62 @@
+from django.conf import settings
+from django.shortcuts import render_to_response
+from django.db.models import Count, Sum
+
+from proj.gather.models import UserProfile, Point, Debt
+from proj.utils import json_response, get_POST_data
+
+import simplejson as json
+import os
+
+def points(request):
+  data = [p.to_json() for p in Point.objects.all()]
+  return json_response(data, 200)
+
+def get_map_data():
+  total_amount = Debt.objects.all().aggregate(Sum('amount'))
+
+  query = Point.objects.annotate(
+      num_users=Count('userprofile'),
+      sum_amount=Sum('userprofile__user__debt__amount') # o fuck
+    ).filter(num_users__gte=1)
+
+  points = []
+  for p in query:
+    if p.sum_amount:
+    points.append(p.to_json())
+
+  # TODO: loading them and then dumping them again is
+  # probably costing us performance (karissa)
+  return {
+    'total_amount': total_amount['amount__sum'],
+    'points': points
+  }
+
+def generate_and_store_map_data():
+    data = get_map_data()
+    static_dir = settings.STATIC_ROOT
+    fp = open("%s/%s.json" % (os.path.join(static_dir, 'js'), "map_data"), 'wb')
+    fp.write(json.dumps(data))
+    fp.close()
+    return data
+
+def map_data(request):
+  """
+  GET /map_data
+  """
+  data = get_map_data()
+  return json_response(data, 200)
+
+
+def generate_map_json(request):
+  """
+  GET /generate_map_json
+  write the map data json to file
+  """
+  password = request.GET.get('password')
+  if password == settings.MAP_PASSWORD:
+    data = generate_and_store_map_data()
+    return json_response(data, 200)
+  else:
+    return json_response({'status': 'error'}, 500)
+
