ac0d5cd75563224dc87bf0a26786e5a8438d3c71
Karissa McKelvey <krmckelv@gmail.com>
2015-01-05 23:53:21 -0800

    Add dynamic updating strikers

--- be/proj/static/js/corinthianCtrl.js
+++ be/proj/static/js/corinthianCtrl.js
@@ -1,7 +1,21 @@
 app.controller('corinthianCtrl',
-  function ($scope, $http, $document, util_svc) {
+  function ($scope, $http, $document, $window, util_svc) {
     $scope.debtors = 13415;
     $scope.debt = 354135;
     $scope.money = 24194
 
+    var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1JWrE9ca5aU9NbGHoioVIhsJJhY9r09T7TCQw220XhgM/pubhtml?gid=0&single=true'
+
+    $window.onload = function() {
+      $scope.loading = true;
+      Tabletop.init( { key: public_spreadsheet_url,
+                       callback: showInfo,
+                       simpleSheet: true } )
+    };
+
+
+    function showInfo(data, tabletop) {
+      $scope.profiles = data;
+      $scope.loading = false;
+    }
 });
