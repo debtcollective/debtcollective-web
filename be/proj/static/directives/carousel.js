7c7b96cef36b03c55721c9e3f091b82abd406483
Karissa McKelvey <krmckelv@gmail.com>
2015-01-08 16:51:26 -0800

    Fix carousel css etc

--- be/proj/static/directives/carousel.js
+++ be/proj/static/directives/carousel.js
@@ -4,16 +4,23 @@ app.directive('carousel', function ($window) {
     scope: {
       'data': '=data'
     },
+    replace: true,
     templateUrl: '/static/directives/carousel.html',
     controller: function ($scope) {
       $scope.selectionIndex = 0;
 
-      $scope.prev = function() {
-        $scope.selectionIndex += 1;
+      $scope.prev = function () {
+        $scope.selectionIndex -= 1;
+        if ($scope.selectionIndex < 0) {
+          $scope.selectionIndex = $scope.data.length - 1
+        }
       }
 
-      $scope.next = function() {
+      $scope.next = function () {
         $scope.selectionIndex += 1;
+        if ($scope.selectionIndex >= $scope.data.length) {
+          $scope.selectionIndex = 0
+        }
       }
 
     }
--- be/proj/static/js/corinthianCtrl.js
+++ be/proj/static/js/corinthianCtrl.js
@@ -6,16 +6,23 @@ app.controller('corinthianCtrl',
     $scope.profileIndex = 0;
     $scope.data = [];
 
-    var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1JWrE9ca5aU9NbGHoioVIhsJJhY9r09T7TCQw220XhgM/pubhtml?gid=0&single=true'
-    $scope.loading = true;
-    Tabletop.init( { key: public_spreadsheet_url,
-                     callback: showInfo,
-                     simpleSheet: true } )
+    function populateStrikers() {
+      var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1JWrE9ca5aU9NbGHoioVIhsJJhY9r09T7TCQw220XhgM/pubhtml?gid=0&single=true'
+      $scope.loading = true;
+
+      if (!Tabletop) {
+        return populateStrikers()
+      }
+
+      Tabletop.init( { key: public_spreadsheet_url,
+                       callback: showInfo,
+                       simpleSheet: true } )
+    }
 
     function showInfo(data) {
       $scope.loading = false;
       $scope.data = data;
-      console.log(data)
     }
 
+    populateStrikers();
 });
