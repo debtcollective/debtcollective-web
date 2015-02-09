50d8bbf6f1c6f79b96f9e4fe24b9e8550376adef
Karissa McKelvey <krmckelv@gmail.com>
2015-02-08 21:25:41 -0800

    fix

--- be/proj/static/js/splashCtrl.js
+++ be/proj/static/js/splashCtrl.js
@@ -30,26 +30,10 @@ app.controller('splashCtrl',
         }, 500)
     }
 
-    // create counter
-    var counterOptions = {
-      increment: 30000,
-      delayTime: 500,
-      counterStart: 0,
-      counterEnd: 0,
-      numbersImage: 'http://rollingjubilee.org/assets/img/jodometer-numbers-24pt.png',
-      widthNumber: 32,
-      heightNumber: 54,
-      spaceNumbers: 0,
-      offsetRight: -10,
-      maxDigits: 10,
-      prefixChar: true
-    }
-
     $http.get('/static/js/map_data.json').then(function (resp) {
         var total_amount = resp.data.total_amount;
-        counterOptions.counterEnd = total_amount;
-        counterOptions.counterStart = total_amount - 30000;
-        $('.counter').jOdometer(counterOptions);
+        odometer.innerHTML = total_amount;
+        //counterOptions.counterStart = total_amount - 30000;
     });
 
 });
