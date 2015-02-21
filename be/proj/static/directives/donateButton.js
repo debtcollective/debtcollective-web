53e557455495023b6a4ddb974ebce04658087a59
Karissa McKelvey <krmckelv@gmail.com>
2015-02-20 16:29:53 -0800

    Add donations

---
+++ be/proj/static/directives/donateButton.js
@@ -0,0 +1,43 @@
+app.directive('donateButton', function ($window) {
+  return {
+    restrict: 'E',
+    scope: {
+      'amount': '@',
+      'flexbile': '='
+    },
+    transclude: true,
+    replace: true,
+    templateUrl: '/static/directives/donatebutton.html',
+    controller: function ($scope, $element, $attrs) {
+      var handler = StripeCheckout.configure({
+        key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh'
+      });
+
+      $scope.flexible = !!$attrs.flexible
+      $scope.form = {
+        flexibleAmount: 10
+      }
+
+      var name = 'Donate'
+      var panel = 'Donate'
+
+      $element.find('button').on('click', function(e) {
+        if ($scope.flexible) $scope.amount = $scope.form.flexibleAmount
+        var description = "$" + $scope.amount + " to the strike fund!"
+
+        handler.open({
+          name: name,
+          description: description,
+          amount: $scope.amount * 100,
+          panelLabel: panel
+        });
+        e.preventDefault();
+      });
+
+      // Close Checkout on page navigation
+      $window.addEventListener('popstate', function() {
+        handler.close();
+      });
+    }
+  }
+})
