7d3b986bd48f9129e6f76fd213dcd5a46353694e
Karissa McKelvey <krmckelv@gmail.com>
2015-03-15 19:33:56 -0700

    TST: Add boto tests for dtr

--- be/proj/arcs/tests.py
+++ be/proj/arcs/tests.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 
 import proj.arcs.dtr as dtr
 
+TEST_BUCKET = 'corinthiandtr.dev'
+conn = get_s3_conn()
+bucket = conn.get_bucket(TEST_BUCKET)
+
 class TestDTR(TestCase):
 
     def test_generate(self):
@@ -16,24 +20,71 @@ class TestDTR(TestCase):
       user = User.objects.get(username='test')
       self.assertEqual('test', user.username)
 
-      TEST_BUCKET = 'corinthiandtr.dev'
-
       key = dtr.generate_for_user(user, {
         'name': 'this is awesome'
       })
 
       self.assertEqual(key, user.id)
 
-      conn = get_s3_conn()
-      bucket = conn.get_bucket(TEST_BUCKET)
       s3_key = bucket.get_key(key)
 
+      # contents exist
       contents = s3_key.get_contents_as_string()
       self.assertTrue(len(contents) > 0)
 
+      # name metadata exists
       name = s3_key.get_metadata('name')
       self.assertEqual(name, 'this is awesome')
 
+      #cleanup
+      bucket.delete_key(key)
+
+    def test_generate_two_users(self):
+      rs = self.client.post('/signup/',
+          {'username': 'test', 'password': 'anoyther'})
+      self.assertEqual(rs.status_code, 200)
+
+      rs = self.client.post('/signup/',
+          {'username': 'test2', 'password': 'anoyther'})
+      self.assertEqual(rs.status_code, 200)
+
+      user = User.objects.get(username='test')
+      self.assertEqual('test', user.username)
+      user_two = User.objects.get(username='test2')
+      self.assertEqual('test2', user_two.username)
+
+      key = dtr.generate_for_user(user, {
+        'name': 'i am the first user'
+      })
+
+      key_two = dtr.generate_for_user(user_two, {
+        'name': 'i am a second user'
+      })
+
+      self.assertEqual(key, user.id)
+      self.assertEqual(key_two, user_two.id)
+
+      s3_key = bucket.get_key(key)
+      s3_key_two = bucket.get_key(key_two)
+
+      # both contents exist
+      contents = s3_key.get_contents_as_string()
+      self.assertTrue(len(contents) > 0)
+
+      contents_two = s3_key_two.get_contents_as_string()
+      self.assertTrue(len(contents) > 0)
+
+      # and are different!
+      self.assertNotEqual(contents, contents_two)
+
+      # metadata keys are different
+      name = s3_key.get_metadata('name')
+      self.assertEqual(name, 'i am the first user')
+
+      name = s3_key_two.get_metadata('name')
+      self.assertEqual(name, 'i am a second user')
 
 
-
+      # cleanup
+      bucket.delete_key(key)
+      bucket.delete_key(key_two)
