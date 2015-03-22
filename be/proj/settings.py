87801168b2e0d14a7b993d38ab44410cd481e138
Karissa McKelvey <krmckelv@gmail.com>
2015-03-22 16:00:10 -0700

    Use static instead of assets for dc wizard portion

--- be/proj/settings.py
+++ be/proj/settings.py
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
 )
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'templates/debtcollective-wizard/assets')
+    os.path.join(BASE_DIR, 'templates/debtcollective-wizard/static')
 )
 
 # Misc.
