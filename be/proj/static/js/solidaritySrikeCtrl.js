7a4aadebbd22264edcbb2ce9c307a6c0125c2a00
Karissa McKelvey <krmckelv@gmail.com>
2015-05-19 16:17:25 -0700

    fix janky loading on solidarity strike page

--- be/proj/static/js/solidaritySrikeCtrl.js
+++ be/proj/static/js/solidaritySrikeCtrl.js
@@ -37,6 +37,7 @@ app.controller('solidarityStrikeCtrl',
           var json = this.toJSON();
           $scope.num = json.length;
           $scope.solidarityStrikers = json.chunk(4)
+          $scope.doneLoading = true
         },
         error : function() {
           console.log("Are you sure you are connected to the internet?");
