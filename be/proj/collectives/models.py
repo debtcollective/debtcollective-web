2b1b0db3c18f19f138b29948100f7915cd1aba15
Karissa McKelvey <krmckelv@gmail.com>
2015-07-17 15:40:01 -0400

    Add features/not featured to action

--- be/proj/collectives/models.py
+++ be/proj/collectives/models.py
@@ -12,6 +12,7 @@ class Action(models.Model):
   link = models.CharField(max_length=150, null=True, blank=True)
   image = models.CharField(max_length=150, null=True, blank=True)
   active = models.BooleanField(default=True)
+  featured = models.BooleanField(default=False)
 
   def __unicode__(self):
     return self.name
